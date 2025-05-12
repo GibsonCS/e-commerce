e922af83b5028c9fea9cf12a2e8efb79991926b5
GibsonCS <gibson.cruzds@gmail.com>
2025-05-12 15:45:22 -0300

    style: finalize admin pagem

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,10 @@
+import Aside from "@/components/Aside";
 
-export default function AdminPage () {
-    return <div>
-        Admin page
+export default function AdminPage() {
+  return (
+    <div className="flex h-full">
+      <Aside />
+      {/* <div className="flex  border bg-amber-400">dsds</div> */}
     </div>
+  );
 }
