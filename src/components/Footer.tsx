8ed88a49532a72c0dc252a663f11f5318ba0f1ca
GibsonCS <gibson.cruzds@gmail.com>
2025-05-12 15:44:20 -0300

    style: finalize footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,10 @@
-import Link from "next/link"
-import { Mail, MapPin, Phone } from "lucide-react"
-
+import Image from "next/image";
+import logo from "@/assets/images/logo.svg";
 
 export function Footer() {
   return (
-    <footer className="bg-slate-900 text-slate-200">
-      <div>
-        Main footer
-      </div>
+    <footer className="flex p-6 justify-center bg-gradient-to-b from-gray-900 to-gray-700 border-t border-gray-800">
+      <Image className="w-16 opacity-15" src={logo} alt="Legal Course Logo" />
     </footer>
-  )
+  );
 }
