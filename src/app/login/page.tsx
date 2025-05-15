ed2fc5e29e9751bb9233418ff88fdc56bc33efc9
GibsonCS <gibson.cruzds@gmail.com>
2025-05-14 22:52:56 -0300

    feat: implement login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
+
   return (
     <main className="flex-1 flex justify-center items-center">
       <LoginForm />
