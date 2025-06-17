aace9eaf87a7d8c358f65870b8f961fb90537fb2
GibsonCS <gibson.cruzds@gmail.com>
2025-06-17 11:43:36 -0300

    feat: add src/app/sobre-nos/

---
+++ src/app/sobre-nos/page.tsx
@@ -0,0 +1,298 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  AcademicCapIcon,
+  HandRaisedIcon,
+  RocketLaunchIcon,
+  SparklesIcon,
+  SunIcon,
+  UserGroupIcon,
+} from "@heroicons/react/20/solid";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const navigation = [
+  { name: "Início", href: "/" },
+  { name: "Cursos", href: "/cursos" },
+  { name: "Professores", href: "/professores" },
+  { name: "Contato", href: "/contato" },
+  { name: "Sobre nós", href: "/sobre-nos" },
+];
+const stats = [
+  { label: "Business was founded", value: "2012" },
+  { label: "People on the team", value: "120+" },
+  { label: "Users on the platform", value: "250k" },
+  { label: "Paid out to creators", value: "$70M" },
+];
+const values = [
+  {
+    name: "Be world-class.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
+    icon: RocketLaunchIcon,
+  },
+  {
+    name: "Take responsibility.",
+    description:
+      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+    icon: HandRaisedIcon,
+  },
+  {
+    name: "Be supportive.",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.",
+    icon: UserGroupIcon,
+  },
+  {
+    name: "Always learning.",
+    description:
+      "Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.",
+    icon: AcademicCapIcon,
+  },
+  {
+    name: "Share everything you know.",
+    description:
+      "Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.",
+    icon: SparklesIcon,
+  },
+  {
+    name: "Enjoy downtime.",
+    description:
+      "Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.",
+    icon: SunIcon,
+  },
+];
+
+export default function AboutPage() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setScroll(false);
+        console.log("kkk");
+      } else if (window.scrollY >= 92) {
+        setScroll(true);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="bg-gray-900">
+      <header
+        className={` absolute inset-x-0 top-0 z-50 ${
+          scroll === true ? "fixed opacity-80 bg-gray-950" : ""
+        } `}
+      >
+        <nav
+          aria-label="Global"
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        >
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Extude</span>
+              <Image
+                alt=""
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                height={50}
+                width={50}
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm/6 font-semibold text-white"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <Link href="/login" className="text-sm/6 font-semibold text-white">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </nav>
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <Image
+                  alt=""
+                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                  className="h-8 w-auto"
+                />
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-400"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/25">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-800"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-gray-800"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+
+      <main className="relative isolate">
+        {/* Background */}
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-4 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+            }}
+            className="aspect-1108/632 w-277 flex-none bg-linear-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+          />
+        </div>
+
+        {/* Header section */}
+        <div className="px-6 pt-14 lg:px-8">
+          <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
+            <h1 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
+              We love creators
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat.
+            </p>
+          </div>
+        </div>
+
+        {/* Content section */}
+        <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+            <div className="grid max-w-xl grid-cols-1 gap-8 text-base/7 text-gray-300 lg:max-w-none lg:grid-cols-2">
+              <div>
+                <p>
+                  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+                  risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                  Id dolor praesent donec est. Odio penatibus risus viverra
+                  tellus varius sit neque erat velit. Faucibus commodo massa
+                  rhoncus, volutpat. Dignissim sed eget risus enim. Mattis
+                  mauris semper sed amet vitae sed turpis id.
+                </p>
+                <p className="mt-8">
+                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien
+                  duis odio id et. Id blandit molestie auctor fermentum
+                  dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
+                  varius vulputate et ultrices hac adipiscing egestas.
+                </p>
+              </div>
+              <div>
+                <p>
+                  Erat pellentesque dictumst ligula porttitor risus eget et
+                  eget. Ultricies tellus felis id dignissim eget. Est augue
+                  maecenas risus nulla ultrices congue nunc tortor. Enim et
+                  nesciunt doloremque nesciunt voluptate.
+                </p>
+                <p className="mt-8">
+                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien
+                  duis odio id et. Id blandit molestie auctor fermentum
+                  dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
+                  varius vulputate et ultrices hac adipiscing egestas. Iaculis
+                  convallis ac tempor et ut. Ac lorem vel integer orci.
+                </p>
+              </div>
+            </div>
+            <dl className="mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4">
+              {stats.map((stat, statIdx) => (
+                <div
+                  key={statIdx}
+                  className="flex flex-col-reverse gap-y-3 border-l border-white/20 pl-6"
+                >
+                  <dt className="text-base/7 text-gray-300">{stat.label}</dt>
+                  <dd className="text-3xl font-semibold tracking-tight text-white">
+                    {stat.value}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+
+        {/* Values section */}
+        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
+              Our values
+            </h2>
+            <p className="mt-6 text-lg/8 text-gray-300">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis.
+            </p>
+          </div>
+          <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base/7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-16">
+            {values.map((value) => (
+              <div key={value.name} className="relative pl-9">
+                <dt className="inline font-semibold text-white">
+                  <value.icon
+                    aria-hidden="true"
+                    className="absolute top-1 left-1 size-5 text-indigo-500"
+                  />
+                  {value.name}
+                </dt>{" "}
+                <dd className="inline">{value.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </main>
+    </div>
+  );
+}
